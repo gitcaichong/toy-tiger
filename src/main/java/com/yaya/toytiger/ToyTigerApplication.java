1eb67bc84e6e301eb15fc0408615defe207ca29c
367752 <chengyifan@deppon.com>
2019-09-20 10:00:28 +0800

    Initial commit

---
+++ src/main/java/com/yaya/toytiger/ToyTigerApplication.java
@@ -0,0 +1,13 @@
+package com.yaya.toytiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyTigerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyTigerApplication.class, args);
+	}
+
+}
